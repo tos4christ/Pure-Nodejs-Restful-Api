6e7933fdfa51cba2743677337a2480e74517097a
tos4christ <gnztrades@gmail.com>
2021-06-23 20:21:19 +0100

    Api to check for server status built to the API connection level

--- config.mjs
+++ config.mjs
@@ -10,7 +10,13 @@ environments.staging = {
     'httpPort' : 3000,
     'httpsPort' : 3001,
     'envName' : 'staging',
-    'hashingSecret': 'tos4christ'
+    'hashingSecret': 'tos4christ',
+    'maxChecks' : 5,
+    'twilio' : {
+        'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+        'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+        'fromPhone' : '+15005550006'
+    }
 }
 
 // Production environment
@@ -18,7 +24,13 @@ environments.production = {
     'httpPort' : 5000,
     'httpsPort' : 5001,
     'envName' : 'production',
-    'hashingSecret': 'tos4christ'
+    'hashingSecret': 'tos4christ',
+    'maxChecks' : 10,
+    'twilio' : {
+        'accountSid' : '',
+        'authToken' : '',
+        'fromPhone' : ''
+    }
 }
 
 // Determine which environment was passed as command line
